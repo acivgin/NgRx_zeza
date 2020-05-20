5a81f9683bb9de17209a27621babd47752c18f5b
Adis Civgin <civgin.adis@hotmail.com>
2020-05-20 12:21:06 +0200

    JSON server and Service added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { StoreModule } from '@ngrx/store';
 import { ShoppingReducer } from './store/reducers/shoppping.reducer';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     StoreModule.forRoot({ shopping: ShoppingReducer }),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
